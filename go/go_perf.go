e935a9a20cc032a3448f31d3bcf1503ad36b92ec
admix <admix.snurnikov@gmail.com>
2015-01-21 23:23:49 -0500

    added go prime num func

--- go/go_perf.go
+++ go/go_perf.go
@@ -1,8 +1,35 @@
 // Low level performance benchmarking using Go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "testing"
+)
+
+func regularArray(maxNum int) {
+	var i int = 2
+	var j int
+
+	var Data = make([]bool, maxNum+1)
+
+	for i := 2; i <= maxNum; i++ {
+		Data[i] = true
+		fmt.Println(i)
+	}
+	i = 2
+	for i; i <= maxNum; i++ {
+		if Data[i] {
+			for j = i + i; j <= maxNum; j += i {
+				Data[j] = false
+			}
+		}
+	}
+	fmt.Println(Data)
+}
 
 func main() {
 	fmt.Println("Low level performance using Go")
+	// t := testing.Benchmark(regularArray(100000))
+	regularArray(100000)
+	// fmt.Println(t)
 }
